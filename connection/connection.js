9437a48017cf9e45bf00a6af3f3173d45ab70d5e
Ashraful Islam Sheiblu <sheiblu@yahoo.com>
2019-10-04 01:57:31 +0600

    Update connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "fulbondhu"
+    database: "tablea"
 });
 
 module.exports = {
